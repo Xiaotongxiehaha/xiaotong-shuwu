e44e19c2d28e3c552d55cd2775cfa334a6ec2b73
Xiaotongxiehaha <vzhdcn48jz@privaterelay.appleid.com>
2026-08-16 14:20:54 +0800

    v2.4.2: 反馈系统+掌握情况独立区块+个人反馈去课后作业+知识点OCR+班级反馈完善

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kgj-v13';
+const CACHE = 'kgj-v19';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
