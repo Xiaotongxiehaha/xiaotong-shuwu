1ad180ec5f811ccc321f273ef5c3dc324aa63a56
Xiaotongxiehaha <vzhdcn48jz@privaterelay.appleid.com>
2026-09-03 11:27:19 +0800

    v2.4.4 cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kgj-v20';
+const CACHE = 'kgj-v21';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
